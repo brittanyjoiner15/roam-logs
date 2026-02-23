c7fc1bc870347d9238f3bd2379c18ce1d1018543
Brittany Joiner <brittanyjoiner15@gmail.com>
2026-02-22 21:38:29 -0500

    add mixpanel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import BottomNavWrapper from '@/components/BottomNavWrapper'
+import MixpanelProvider from '@/components/MixpanelProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <MixpanelProvider />
         <div className="pb-16">
           {children}
         </div>
